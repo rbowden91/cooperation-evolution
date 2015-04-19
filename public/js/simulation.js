98de6db087501f776f9b14b99245db05a018aa21
Rob Bowden <rbowden91@gmail.com>
2015-04-19 19:09:29 -0400

    direct reciprocity seems all set

--- public/js/simulation.js
+++ public/js/simulation.js
@@ -53,44 +53,48 @@ function runSimulation(population) {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     for (var i = 0; i < population.length; i+=2) {
-	population[i].draw(context);
-	population[i+1].draw(context);
+        population[i].draw(context);
+        population[i+1].draw(context);
 
-	if (population[i] instanceof Cooperator && population[i+1] instanceof Defector) {
-	    population[i + 1].fitness += parameters['benefit'];
-	    population[i].fitness -= parameters['cost'];
-	}
-	else if (population[i] instanceof Defector && population[i+1] instanceof Cooperator) {
-	    population[i].fitness += parameters['benefit'];
-	    population[i + 1].fitness -= parameters['cost'];
-	}
-	else if (population[i] instanceof Cooperator && population[i+1] instanceof Cooperator) {
-	    do {
-		population[i + 1].fitness += parameters['benefit'];
-		population[i].fitness -= parameters['cost'];
-	    } while (Math.random() < parameters['repeat_probability']);
-	}
+        if (population[i] instanceof Cooperator && population[i+1] instanceof Defector) {
+            population[i + 1].fitness += parameters['benefit'];
+            population[i].fitness -= parameters['cost'];
+        }
+        else if (population[i] instanceof Defector && population[i+1] instanceof Cooperator) {
+            population[i].fitness += parameters['benefit'];
+            population[i + 1].fitness -= parameters['cost'];
+        }
+        else if (population[i] instanceof Cooperator && population[i+1] instanceof Cooperator) {
+            do {
+                population[i + 1].fitness += parameters['benefit'];
+                population[i].fitness -= parameters['cost'];
+            } while (Math.random() < parameters['repeat_probability']);
+            console.log(population[i+1].fitness);
+        }
     }
 
     // if an odd number, then the last organism gets left off. draw it, though.
     if (population.length % 2 !== 0) {
-    	population[population.length - 1].draw(context);
+        population[population.length - 1].draw(context);
     }
 
     // use a roulette wheel strategy to pick offspring. a naive O(n) implementation
-    var total_fitness = population.length * parameters['starting_fitness'];
+    var total_fitness = 0;
+    for (var i = 0; i < population.length; i++) {
+    	total_fitness += population[i].fitness;
+    }
 
     var new_population = [];
     for (var i = 0; i < population.length; i++) {
-    	var selection = Math.random() * total_fitness;
-    	for (var j = 0; j < population.length; j++) {
-	    selection -= population[j].fitness;
-    	    if (selection <= 0) {
-    	    	var c = population[j] instanceof Cooperator ? Cooperator : Defector;
-		new_population.push(new c(population[i].xposition, population[i].yposition, parameters['starting_fitness']));
-    	    	break;
-	    }
-	}
+        var selection = Math.random() * total_fitness;
+        for (var j = 0; j < population.length; j++) {
+            selection -= population[j].fitness;
+            if (selection <= 0) {
+                var c = population[j] instanceof Cooperator ? Cooperator : Defector;
+                new_population.push(new c(population[i].xposition, population[i].yposition, parameters['starting_fitness']));
+                break;
+            }
+        }
     }
 
     simulation_loop = setTimeout(function() { runSimulation(new_population); }, parameters.delay);
@@ -98,14 +102,14 @@ function runSimulation(population) {
 
 
 $(function() {
-	var pageHeight = $(window).height();
-	var pageWidth = $(window).width();
-	var divHeight = $('#top').height();
+        var pageHeight = $(window).height();
+        var pageWidth = $(window).width();
+        var divHeight = $('#top').height();
     $('#rest').html($('<canvas id="simulation" width="' + pageWidth + '" height="' + (pageHeight - divHeight - 10)  + '">'));
 
 
     for (i in parameters) {
-    	$('#' + i).val(parameters[i]);
+        $('#' + i).val(parameters[i]);
     }
 
     $('form').submit(function(){
@@ -114,51 +118,40 @@ $(function() {
 
     $('#run_simulation').on('click', function() {
 
-    	// end a simulation
-	if (simulation_loop !== false) {
-	    clearTimeout(simulation_loop);
-	    simulation_loop = false;
+        // end a simulation
+        if (simulation_loop !== false) {
+            clearTimeout(simulation_loop);
+            simulation_loop = false;
 
-	    for (i in parameters) {
-		$('#' + i).prop('disabled', false);
-	    }
+            for (i in parameters) {
+                $('#' + i).prop('disabled', false);
+            }
 
-	    $('#run_simulation').text('Run');
-	    return false;
-	}
+            $('#run_simulation').text('Run');
+            return false;
+        }
 
-    	// TODO: error check numbers
-    	for (i in parameters) {
-    	    parameters[i] = parseFloat($('#' + i).val());
-	    $('#' + i).prop('disabled', true);
-	}
+        // TODO: error check numbers
+        for (i in parameters) {
+            parameters[i] = parseFloat($('#' + i).val());
+            $('#' + i).prop('disabled', true);
+        }
 
-	var population = [];
+        var population = [];
 
-	var num_cooperators = Math.floor(parameters['num_organisms'] * parameters['cooperator_fraction']);
+        var num_cooperators = Math.floor(parameters['num_organisms'] * parameters['cooperator_fraction']);
 
-	var width = $('#simulation').width();
-	var height = $('#simulation').height();
-	for (var i = 0; i < parameters['num_organisms']; i++) {
-	    // XXX wildly inefficient -- find somewhere to place the organism
-	    var too_close = true;
-	    while (too_close) {
-	    	too_close = false;
-		var x = Math.random() * (width - 2 * radius) + radius;
-		var y = Math.random() * (height - 2 * radius) + radius;
-		for (var j = 0; j < population.length; j++) {
-		    if (Math.pow(population[j].xposition - x, 2) + Math.pow(population[j].yposition - y, 2) <
-		    	Math.pow(radius * 4, 2))
-		    	too_close = true;
-		    	break;
-		}
-	    }
-	    var o = new (i < num_cooperators ? Cooperator : Defector)(x, y, parameters['starting_fitness']);
-	    population.push(o);
-	}
+        var width = $('#simulation').width();
+        var height = $('#simulation').height();
+        for (var i = 0; i < parameters['num_organisms']; i++) {
+            var x = Math.random() * (width - 2 * radius) + radius;
+            var y = Math.random() * (height - 2 * radius) + radius;
+            var o = new (i < num_cooperators ? Cooperator : Defector)(x, y, parameters['starting_fitness']);
+            population.push(o);
+        }
 
-	$('#run_simulation').text('Stop');
+        $('#run_simulation').text('Stop');
 
-	runSimulation(population);
+        runSimulation(population);
     });
 });
